e7d0af7a7f9c60d4e8c5bccc0d180005af82dc79
vivekjoy <vivek.joy@litmus7.com>
2020-06-15 20:32:34 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,350 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+import { findDOMNode } from "react-dom";
+
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import ReactPlayer from "react-player";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+
+import Slider from "@material-ui/core/Slider";
+import Tooltip from "@material-ui/core/Tooltip";
+import Grid from "@material-ui/core/Grid";
+import VolumeUp from "@material-ui/icons/VolumeUp";
+import VolumeDown from "@material-ui/icons/VolumeDown";
+import VolumeMute from "@material-ui/icons/VolumeOff";
+import FullScreen from "@material-ui/icons/Fullscreen";
+import Popover from "@material-ui/core/Popover";
+import screenful from "screenfull";
+import Controls from "./components/Controls";
+
+const useStyles = makeStyles((theme) => ({
+  playerWrapper: {
+    width: "100%",
+    position: "relative",
+    // "&:hover": {
+    //   "& $controlsWrapper": {
+    //     visibility: "visible",
+    //   },
+    // },
+  },
+
+  controlsWrapper: {
+    visibility: "hidden",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    background: "rgba(0,0,0,0.4)",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  topControls: {
+    display: "flex",
+    justifyContent: "flex-end",
+    padding: theme.spacing(2),
+  },
+  middleControls: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  bottomWrapper: {
+    display: "flex",
+    flexDirection: "column",
+
+    // background: "rgba(0,0,0,0.6)",
+    // height: 60,
+    padding: theme.spacing(2),
+  },
+
+  bottomControls: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    // height:40,
+  },
+
+  button: {
+    margin: theme.spacing(1),
+  },
+  controlIcons: {
+    color: "#777",
+
+    fontSize: 50,
+    transform: "scale(0.9)",
+    "&:hover": {
+      color: "#fff",
+      transform: "scale(1)",
+    },
+  },
+
+  bottomIcons: {
+    color: "#999",
+    "&:hover": {
+      color: "#fff",
+    },
+  },
+
+  volumeSlider: {
+    width: 100,
+  },
+}));
+
+const PrettoSlider = withStyles({
+  root: {
+    height: 8,
+  },
+  thumb: {
+    height: 24,
+    width: 24,
+    backgroundColor: "#fff",
+    border: "2px solid currentColor",
+    marginTop: -8,
+    marginLeft: -12,
+    "&:focus, &:hover, &$active": {
+      boxShadow: "inherit",
+    },
+  },
+  active: {},
+  valueLabel: {
+    left: "calc(-50% + 4px)",
+  },
+  track: {
+    height: 8,
+    borderRadius: 4,
+  },
+  rail: {
+    height: 8,
+    borderRadius: 4,
+  },
+})(Slider);
+
+function ValueLabelComponent(props) {
+  const { children, open, value } = props;
+
+  return (
+    <Tooltip open={open} enterTouchDelay={0} placement="top" title={value}>
+      {children}
+    </Tooltip>
+  );
+}
+
+const format = (seconds) => {
+  if (isNaN(seconds)) {
+    return `00:00`;
+  }
+  const date = new Date(seconds * 1000);
+  const hh = date.getUTCHours();
+  const mm = date.getUTCMinutes();
+  const ss = date.getUTCSeconds().toString().padStart(2, "0");
+  if (hh) {
+    return `${hh}:${mm.toString().padStart(2, "0")}:${ss}`;
+  }
+  return `${mm}:${ss}`;
+};
+
+let count = 0;
 
 function App() {
+  const classes = useStyles();
+  const [showControls, setShowControls] = useState(false);
+  // const [count, setCount] = useState(0);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [timeDisplayFormat, setTimeDisplayFormat] = React.useState("normal");
+  const [state, setState] = useState({
+    pip: false,
+    playing: true,
+    controls: false,
+    light: false,
+
+    muted: true,
+    played: 0,
+    duration: 0,
+    playbackRate: 1.0,
+    volume: 1,
+    loop: false,
+    seeking: false,
+  });
+
+  const playerRef = useRef(null);
+  const playerContainerRef = useRef(null);
+  const controlsRef = useRef(null);
+
+  const {
+    playing,
+    controls,
+    light,
+
+    muted,
+    loop,
+    playbackRate,
+    pip,
+    played,
+    seeking,
+    volume,
+  } = state;
+
+  const handlePlayPause = () => {
+    setState({ ...state, playing: !state.playing });
+  };
+
+  const handleRewind = () => {
+    playerRef.current.seekTo(playerRef.current.getCurrentTime() - 10);
+  };
+
+  const handleFastForward = () => {
+    playerRef.current.seekTo(playerRef.current.getCurrentTime() + 10);
+  };
+
+  const handleProgress = (changeState) => {
+    if (count > 3) {
+      controlsRef.current.style.visibility = "hidden";
+      count = 0;
+    }
+    if (controlsRef.current.style.visibility == "visible") {
+      count += 1;
+    }
+    if (!state.seeking) {
+      setState({ ...state, ...changeState });
+    }
+  };
+
+  const handleSeekChange = (e, newValue) => {
+    console.log({ newValue });
+    setState({ ...state, played: parseFloat(newValue / 100) });
+  };
+
+  const handleSeekMouseDown = (e) => {
+    setState({ ...state, seeking: true });
+  };
+
+  const handleSeekMouseUp = (e, newValue) => {
+    console.log({ value: e.target });
+    setState({ ...state, seeking: false });
+    // console.log(sliderRef.current.value)
+    playerRef.current.seekTo(newValue / 100, "fraction");
+  };
+
+  const handleDuration = (duration) => {
+    setState({ ...state, duration });
+  };
+
+  const handleVolumeSeekDown = (e, newValue) => {
+    setState({ ...state, seeking: false, volume: parseFloat(newValue / 100) });
+  };
+  const handleVolumeChange = (e, newValue) => {
+    // console.log(newValue);
+    setState({
+      ...state,
+      volume: parseFloat(newValue / 100),
+      muted: newValue === 0 ? true : false,
+    });
+  };
+
+  const toggleFullScreen = () => {
+    screenful.toggle(playerContainerRef.current);
+  };
+
+  const handleMouseMove = () => {
+    console.log("mousemove");
+    controlsRef.current.style.visibility = "visible";
+    count = 0;
+  };
+
+  const hanldeMouseLeave = () => {
+    controlsRef.current.style.visibility = "hidden";
+    count = 0;
+  };
+
+  const handleDisplayFormat = () => {
+    setTimeDisplayFormat(
+      timeDisplayFormat == "normal" ? "remaining" : "normal"
+    );
+  };
+
+  const handlePlaybackRate = (rate) => {
+    setState({ ...state, playbackRate: rate });
+  };
+
+  const hanldeMute = () => {
+    setState({ ...state, muted: !state.muted });
+  };
+
+  const currentTime =
+    playerRef && playerRef.current
+      ? playerRef.current.getCurrentTime()
+      : "00:00";
+
+  const duration =
+    playerRef && playerRef.current ? playerRef.current.getDuration() : "00:00";
+  const elapsedTime =
+    timeDisplayFormat == "normal"
+      ? format(currentTime)
+      : `-${format(duration - currentTime)}`;
+
+  const totalDuration = format(duration);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography>React Video Player</Typography>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+      <Container maxWidth="md">
+        <div
+          onMouseMove={handleMouseMove}
+          onMouseLeave={hanldeMouseLeave}
+          ref={playerContainerRef}
+          className={classes.playerWrapper}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <ReactPlayer
+            ref={playerRef}
+            width="100%"
+            height="100%"
+            url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4"
+            pip={pip}
+            playing={playing}
+            controls={false}
+            light={light}
+            loop={loop}
+            playbackRate={playbackRate}
+            volume={volume}
+            muted={muted}
+            onProgress={handleProgress}
+          />
+
+          <Controls
+            ref={controlsRef}
+            onSeek={handleSeekChange}
+            onSeekMouseDown={handleSeekMouseDown}
+            onSeekMouseUp={handleSeekMouseUp}
+            onDuration={handleDuration}
+            onRewind={handleRewind}
+            onPlayPause={handlePlayPause}
+            onFastForward={handleFastForward}
+            playing={playing}
+            played={played}
+            elapsedTime={elapsedTime}
+            totalDuration={totalDuration}
+            onMute={hanldeMute}
+            muted={muted}
+            onVolumeChange={handleVolumeChange}
+            onVolumeSeekDown={handleVolumeSeekDown}
+            onChangeDispayFormat={handleDisplayFormat}
+            playbackRate={playbackRate}
+            onPlaybackRateChange={handlePlaybackRate}
+            onToggleFullScreen={toggleFullScreen}
+            volume={volume}
+          />
+        </div>
+      </Container>
+    </>
   );
 }
 
---
+++ src/components/Controls.js
@@ -0,0 +1,352 @@
+import React, { forwardRef, useState } from "react";
+import PropTypes from "prop-types";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import BookmarkIcon from "@material-ui/icons/Bookmark";
+import FastRewindIcon from "@material-ui/icons/FastRewind";
+import FastForwardIcon from "@material-ui/icons/FastForward";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import PauseIcon from "@material-ui/icons/Pause";
+import Slider from "@material-ui/core/Slider";
+import Tooltip from "@material-ui/core/Tooltip";
+import Grid from "@material-ui/core/Grid";
+import VolumeUp from "@material-ui/icons/VolumeUp";
+import VolumeDown from "@material-ui/icons/VolumeDown";
+import VolumeMute from "@material-ui/icons/VolumeOff";
+import FullScreen from "@material-ui/icons/Fullscreen";
+import Popover from "@material-ui/core/Popover";
+
+const useStyles = makeStyles((theme) => ({
+  controlsWrapper: {
+    visibility: "hidden",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 5,
+    background: "rgba(0,0,0,0.4)",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+
+  button: {
+    margin: theme.spacing(1),
+  },
+  controlIcons: {
+    color: "#777",
+
+    fontSize: 50,
+    transform: "scale(0.9)",
+    "&:hover": {
+      color: "#fff",
+      transform: "scale(1)",
+    },
+  },
+
+  bottomIcons: {
+    color: "#999",
+    "&:hover": {
+      color: "#fff",
+    },
+  },
+
+  volumeSlider: {
+    width: 100,
+  },
+}));
+
+const PrettoSlider = withStyles({
+  root: {
+    height: 8,
+  },
+  thumb: {
+    height: 24,
+    width: 24,
+    backgroundColor: "#fff",
+    border: "2px solid currentColor",
+    marginTop: -8,
+    marginLeft: -12,
+    "&:focus, &:hover, &$active": {
+      boxShadow: "inherit",
+    },
+  },
+  active: {},
+  valueLabel: {
+    left: "calc(-50% + 4px)",
+  },
+  track: {
+    height: 8,
+    borderRadius: 4,
+  },
+  rail: {
+    height: 8,
+    borderRadius: 4,
+  },
+})(Slider);
+
+function ValueLabelComponent(props) {
+  const { children, open, value } = props;
+
+  return (
+    <Tooltip open={open} enterTouchDelay={0} placement="top" title={value}>
+      {children}
+    </Tooltip>
+  );
+}
+
+const Controls = forwardRef(
+  (
+    {
+      onSeek,
+      onSeekMouseDown,
+      onSeekMouseUp,
+      onDuration,
+      onRewind,
+      onPlayPause,
+      onFastForward,
+      playing,
+      played,
+      elapsedTime,
+      totalDuration,
+      onMute,
+      muted,
+      onVolumeSeekDown,
+      onChangeDispayFormat,
+      playbackRate,
+      onPlaybackRateChange,
+      onToggleFullScreen,
+      volume,
+      onVolumeChange,
+    },
+    ref
+  ) => {
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+    const id = open ? "simple-popover" : undefined;
+
+    return (
+      <div ref={ref} className={classes.controlsWrapper}>
+        <Grid
+          container
+          direction="column"
+          justify="space-between"
+          style={{ flexGrow: 1 }}
+        >
+          <Grid
+            container
+            direction="row"
+            alignItems="center"
+            justify="flex-end"
+            style
+            style={{ padding: 16 }}
+          >
+            <Grid item>
+              <Button
+                variant="contained"
+                color="primary"
+                startIcon={<BookmarkIcon />}
+              >
+                Bookmark
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid container direction="row" alignItems="center" justify="center">
+            <IconButton
+              onClick={onRewind}
+              className={classes.controlIcons}
+              aria-label="rewind"
+            >
+              <FastRewindIcon
+                className={classes.controlIcons}
+                fontSize="inherit"
+              />
+            </IconButton>
+            <IconButton
+              onClick={onPlayPause}
+              className={classes.controlIcons}
+              aria-label="play"
+            >
+              {playing ? (
+                <PauseIcon fontSize="inherit" />
+              ) : (
+                <PlayArrowIcon fontSize="inherit" />
+              )}
+            </IconButton>
+            <IconButton
+              onClick={onFastForward}
+              className={classes.controlIcons}
+              aria-label="forward"
+            >
+              <FastForwardIcon fontSize="inherit" />
+            </IconButton>
+          </Grid>
+          {/* bottom controls */}
+          <Grid
+            container
+            direction="row"
+            justify="space-between"
+            alignItems="center"
+            style={{ padding: 16 }}
+          >
+            <Grid item xs={12}>
+              <PrettoSlider
+                min={0}
+                max={100}
+                ValueLabelComponent={(props) => (
+                  <ValueLabelComponent {...props} value={elapsedTime} />
+                )}
+                aria-label="custom thumb label"
+                value={played * 100}
+                onChange={onSeek}
+                onMouseDown={onSeekMouseDown}
+                onChangeCommitted={onSeekMouseUp}
+                onDuration={onDuration}
+              />
+            </Grid>
+
+            <Grid item>
+              <Grid container alignItems="center">
+                <IconButton
+                  onClick={onPlayPause}
+                  className={classes.bottomIcons}
+                >
+                  {playing ? (
+                    <PauseIcon fontSize="large" />
+                  ) : (
+                    <PlayArrowIcon fontSize="large" />
+                  )}
+                </IconButton>
+
+                <IconButton
+                  // onClick={() => setState({ ...state, muted: !state.muted })}
+                  onClick={onMute}
+                  className={`${classes.bottomIcons} ${classes.volumeButton}`}
+                >
+                  {muted ? (
+                    <VolumeMute fontSize="large" />
+                  ) : volume > 0.5 ? (
+                    <VolumeUp fontSize="large" />
+                  ) : (
+                    <VolumeDown fontSize="large" />
+                  )}
+                </IconButton>
+
+                <Slider
+                  min={0}
+                  max={100}
+                  value={muted ? 0 : volume * 100}
+                  onChange={onVolumeChange}
+                  aria-labelledby="input-slider"
+                  className={classes.volumeSlider}
+                  onMouseDown={onSeekMouseDown}
+                  onChangeCommitted={onVolumeSeekDown}
+                />
+                <Button
+                  variant="text"
+                  onClick={
+                    onChangeDispayFormat
+                    //     () =>
+                    //   setTimeDisplayFormat(
+                    //     timeDisplayFormat == "normal" ? "remaining" : "normal"
+                    //   )
+                  }
+                >
+                  <Typography
+                    variant="body1"
+                    style={{ color: "#fff", marginLeft: 16 }}
+                  >
+                    {elapsedTime}/{totalDuration}
+                  </Typography>
+                </Button>
+              </Grid>
+            </Grid>
+
+            <Grid item>
+              <Button
+                onClick={handleClick}
+                aria-describedby={id}
+                className={classes.bottomIcons}
+                variant="text"
+              >
+                <Typography>{playbackRate}X</Typography>
+              </Button>
+
+              <Popover
+                container={ref.current}
+                open={open}
+                id={id}
+                onClose={handleClose}
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                transformOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+              >
+                <Grid container direction="column-reverse">
+                  {[0.5, 1, 1.5, 2].map((rate) => (
+                    <Button
+                      key={rate}
+                      //   onClick={() => setState({ ...state, playbackRate: rate })}
+                      onClick={() => onPlaybackRateChange(rate)}
+                      variant="text"
+                    >
+                      <Typography
+                        color={rate === playbackRate ? "secondary" : "inherit"}
+                      >
+                        {rate}X
+                      </Typography>
+                    </Button>
+                  ))}
+                </Grid>
+              </Popover>
+              <IconButton
+                onClick={onToggleFullScreen}
+                className={classes.bottomIcons}
+              >
+                <FullScreen fontSize="large" />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+);
+
+Controls.propTypes = {
+  onSeek: PropTypes.func,
+  onSeekMouseDown: PropTypes.func,
+  onSeekMouseUp: PropTypes.func,
+  onDuration: PropTypes.func,
+  onRewind: PropTypes.func,
+  onPlayPause: PropTypes.func,
+  onFastForward: PropTypes.func,
+  onVolumeSeekDown: PropTypes.func,
+  onChangeDispayFormat: PropTypes.func,
+  onPlaybackRateChange: PropTypes.func,
+  onToggleFullScreen: PropTypes.func,
+  onMute: PropTypes.func,
+  playing: PropTypes.bool,
+  played: PropTypes.number,
+  elapsedTime: PropTypes.string,
+  totalDuration: PropTypes.string,
+  muted: PropTypes.bool,
+  playbackRate: PropTypes.number,
+};
+export default Controls;
